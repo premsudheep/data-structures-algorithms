7b9b3609a2afcb10fae9d963911aa68faea4bf61
Prem Sudheep <36010985+premsudheep@users.noreply.github.com>
2023-08-06 00:10:20 -0400

    added a new DP problem solution

--- src/com/prem/problems/Blind75.java
+++ src/com/prem/problems/Blind75.java
@@ -591,6 +591,60 @@ public class Blind75 {
         return longest;
     }
 
+    // 1143. Longest Common Subsequence
+
+    // https://www.youtube.com/watch?v=NnD96abizww
+
+    // Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+
+    // A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+    // For example, "ace" is a subsequence of "abcde".
+    // A common subsequence of two strings is a subsequence that is common to both strings.
+
+    
+
+    // Example 1:
+
+    // Input: text1 = "abcde", text2 = "ace" 
+    // Output: 3  
+    // Explanation: The longest common subsequence is "ace" and its length is 3.
+    // Example 2:
+
+    // Input: text1 = "abc", text2 = "abc"
+    // Output: 3
+    // Explanation: The longest common subsequence is "abc" and its length is 3.
+    // Example 3:
+
+    // Input: text1 = "abc", text2 = "def"
+    // Output: 0
+    // Explanation: There is no such common subsequence, so the result is 0.
+
+
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        int rows = text1.length();
+        int columns = text2.length();
+        int[][] dp = new int[rows + 1][columns + 1];
+        
+        java.util.Arrays.fill(dp[0], 0);
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], 0);
+        }
+
+        for(int i = 1; i <= rows; i++) {
+            for(int j = 1; j <= columns; j++) {
+                if(text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[rows][columns];
+    }
+
     // 4. Graph
 
     // 133. Clone Graph
